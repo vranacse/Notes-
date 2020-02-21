c3944ee31c9e24506a999763c1e603e94eaea254
Vikas Rana <vikas@tradelab.in>
2020-02-21 20:54:31 +0530

    Implement Room and note view model

---
+++ app/src/main/java/com/vikas/notesforobvious/data/NoteDao.kt
@@ -0,0 +1,26 @@
+package com.vikas.notesforobvious.data
+
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.vikas.notesforobvious.data.Note
+
+@Dao
+interface NoteDao {
+
+    @Insert
+    fun insert(note: Note)
+
+    @Update
+    fun update(note: Note)
+
+    @Delete
+    fun delete(note: Note)
+
+    @Query("DELETE FROM note_table")
+    fun deleteAllNotes()
+
+    @Query("SELECT * FROM note_table ORDER BY priority DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+
+}
