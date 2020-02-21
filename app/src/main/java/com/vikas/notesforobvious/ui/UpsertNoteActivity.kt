83d03bbed9cc55634c301d2a98c8ed7d8dffc046
Vikas Rana <vikas@tradelab.in>
2020-02-21 21:15:55 +0530

    Add fonts

--- app/src/main/java/com/vikas/notesforobvious/ui/UpsertNoteActivity.kt
+++ app/src/main/java/com/vikas/notesforobvious/ui/UpsertNoteActivity.kt
@@ -8,6 +8,6 @@ class UpsertNoteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_upsert)
+        setContentView(R.layout.activity_upsert_note)
     }
 }
